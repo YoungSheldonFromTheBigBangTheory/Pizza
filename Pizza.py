cb0ad63ac2964a4b8760b68ed803d67db4729eab
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-04-01 00:15:20 +0000

    ?

--- Pizza.py
+++ Pizza.py
@@ -1,8 +1,28 @@
 import math
 
-s = int(input("s "))
-t = input("t ")
+
+st = float(0)
+
+while True:
+
+    s = input("Large or Extra Large? ")
+    t = float(input("How many toppings "))
+
+    try:
+        s = str(s)
+    except ValueError:
+        print("Invalid Size. Try Again")
+        continue
+
+    if s.casefold() == "large": 
+        st = 6
+    elif s.casefold() == "extra large":
+        st = 10
+    st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*(t) + 0.15), 2))
+    tax = st*0.13
 
 
-s = 0.0167*(s**3) - 0.1*(s**2) + 0.9333*(s) + 0.15
-print(s)
+    print("\nSubtotal    $" + str(st))
+    print("Tax         $" + str(tax))
+    print("Total       $" + str(round(float(st + tax), 2)))
+    break
