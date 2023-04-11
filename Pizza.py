17ed30475da7267255089e06a1f30a7e9ae41123
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-04-11 13:45:20 +0000

    b

--- Pizza.py
+++ Pizza.py
@@ -2,21 +2,28 @@ import math
 
 st = float(0)
 
-while True:
+s = input("Large or Extra Large? ").lower()
+while s != "large" and s != "extra large":
+    print("\nInvalid. Try Again\n")
+    s = input("Large or Extra Large? ").lower()
+    
+t = (input("How many toppings "))
+t = float(t)
+while t not in range(0, 5):
+    print("\nInvalid. Try Again\n")
+    t = (input("How many toppings "))
+    t = float(t)
 
-    s = input("Large or Extra Large? ")
-    t = float(input("How many toppings "))
+if s.casefold() == "large": 
+    st = 6
+elif s.casefold() == "extra large":
+    st = 10
+if t != 0:
+    st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*(t) + 0.15), 2))
+tax = round(st*0.13, 2)
 
-    if s.casefold() == "large": 
-        st = 6
-    elif s.casefold() == "extra large":
-        st = 10
-    if t != 0:
-        st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*(t) + 0.15), 2))
-    tax = round(st*0.13, 2)
-
-
-    print("\nSubtotal    $" + str(st))
-    print("Tax         $" + str(tax))
-    print("Total       $" + str(round(float(st + tax), 2)))
-    break
+print('\n' * 2 + s + " size")
+print(str(t) + " toppings")
+print("\nSubtotal    $" + str(st))
+print("Tax         $" + str(tax))
+print("Total       $" + str(round(float(st + tax), 2)))
