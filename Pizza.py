deba9943bc4f5d1407c324bd12dea79bab5d66e0
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-04-13 01:12:15 +0000

    b

--- Pizza.py
+++ Pizza.py
@@ -1,19 +1,27 @@
-while True:
+import math
 
-    
+st = float(0)
+z = 0
+
+while True:
 
     s = input("Large or Extra Large? ").lower()
     while s != "large" and s != "extra large" and s != "l" and s != "xl":
         print("\nInvalid. Try Again\n")
         s = input("Large or Extra Large? ").lower()
     
-    
-    t = (input("How many toppings "))
+    while z == 0:
+        try:
+            t = str(input("How many toppings "))
+            if float(t) in range(0, 5) and t.isnumeric() is True:
+                z = 1
+            else:
+                print("\nInvalid. Try Again\n")
+                t = t + t
+        except ValueError:
+            print("\nInvalid. Try Again\n")
+
     t = float(t)
-    while t not in range(0, 5):
-        print("\nInvalid. Try Again\n")
-        t = (input("How many toppings "))
-        t = float(t)
 
     if s.casefold() == "large" or "l": 
         st = 6
@@ -23,7 +31,8 @@ while True:
         st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*(t) + 0.15), 2))
     tax = round(st*0.13, 2)
 
-    print("\nSize ")
+    print('\n' + s + " Size ")
+    print(str(t) + " Toppings")
     print("\nSubtotal    $" + str(st))
     print("Tax         $" + str(tax))
     print("Total       $" + str(round(float(st + tax), 2)))
--- PizzaWorking.py
+++ PizzaWorking.py
@@ -1,53 +1,34 @@
-import math
 
-st = float(0)
-z = 0
 
-while True:
+#st = float(0)
 
-    
-
+s = input("Large or Extra Large? ").lower()
+while s != "large" and s != "extra large" and s != "l" and s != "xl":
+    print("\nInvalid. Try Again\n")
     s = input("Large or Extra Large? ").lower()
-    while s != "large" and s != "extra large" and s != "l" and s != "xl":
-        print("\nInvalid. Try Again\n")
-        s = input("Large or Extra Large? ").lower()
     
-    
-    #t = str(input("How many toppings "))
-    #print (float(t))
-    #t = float(t)
-    #while float(t) not in range(0, 5) and t.isnumeric() is True:
-        #print("\nInvalid. Try Again\n")
-        #t = (input("How many toppings "))
-   
-    while z == 0:
-        try:
-            t = str(input("How many toppings "))
-            if float(t) in range(0, 5) and t.isnumeric() is True:
-                z = 1
-            else:
-                print("\nInvalid. Try Again\n")
-                t = t + t
-        except ValueError:
+while True:
+    try:
+        t = str(input("How many toppings "))
+        if float(t) in range(0, 5) and t.isnumeric() is True:
+            break
+        else:
             print("\nInvalid. Try Again\n")
-            #t = (input("How many toppings "))
-        #else:
-            #z = 1
-            #t = float(t)
+    except ValueError:
+        print("\nInvalid. Try Again\n")
 
+t = float(t)
 
-    t = float(t)
+if s.casefold() == "large" or "l": 
+    st = 6
+elif s.casefold() == "extra large" or "xl":
+    st = 10
+if t != 0:
+    st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*(t) + 0.15), 2))
+tax = round(st*0.13, 2)
 
-    if s.casefold() == "large" or "l": 
-        st = 6
-    elif s.casefold() == "extra large" or "xl":
-        st = 10
-    if t != 0:
-        st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*(t) + 0.15), 2))
-    tax = round(st*0.13, 2)
-
-    print("\nSize ")
-    print("\nSubtotal    $" + str(st))
-    print("Tax         $" + str(tax))
-    print("Total       $" + str(round(float(st + tax), 2)))
-    break
+print('\n' + s + " Size ")
+print(int(t), "Toppings")
+print("\nSubtotal    $" + str(st))
+print("Tax         $" + str(tax))
+print("Total       $" + str(round(float(st + tax), 2)))
