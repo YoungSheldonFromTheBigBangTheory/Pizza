02859ed2c8c64d1f6bb1f089e9b613bcf952c14c
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-04-13 01:26:16 +0000

    p

--- PizzaWorking.py
+++ PizzaWorking.py
@@ -1,33 +1,29 @@
-
-
-#st = float(0)
-
 s = input("Large or Extra Large? ").lower()
 while s != "large" and s != "extra large" and s != "l" and s != "xl":
-    print("\nInvalid. Try Again\n")
+    print("\nInvalid. Please enter a size.\n")
     s = input("Large or Extra Large? ").lower()
     
 while True:
     try:
         t = str(input("How many toppings "))
         if float(t) in range(0, 5) and t.isnumeric() is True:
+            t = float(t)
             break
         else:
-            print("\nInvalid. Try Again\n")
+            print("\nInvalid. Please enter amount, 0-4.\n")
     except ValueError:
-        print("\nInvalid. Try Again\n")
-
-t = float(t)
+        print("\nInvalid. Not an amount.\n")
 
 if s.casefold() == "large" or "l": 
     st = 6
-elif s.casefold() == "extra large" or "xl":
+if s.casefold() == "extra large" or "xl":
     st = 10
+    print(st)
 if t != 0:
     st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*(t) + 0.15), 2))
 tax = round(st*0.13, 2)
 
-print('\n' + s + " Size ")
+print('\n' * 3 + s + " Size ")
 print(int(t), "Toppings")
 print("\nSubtotal    $" + str(st))
 print("Tax         $" + str(tax))
