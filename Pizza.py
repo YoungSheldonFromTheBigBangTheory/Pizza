491e38281800060d410db53b85b5bdebe61d1048
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-04-13 13:41:18 +0000

    o

--- Pizza.py
+++ Pizza.py
@@ -1,39 +1,30 @@
-import math
-
-st = float(0)
-z = 0
-
-while True:
-
+s = input("Large or Extra Large? ").lower()
+while s != "large" and s != "extra large" and s != "l" and s != "xl":
+    print("\nInvalid. Please enter a size.\n")
     s = input("Large or Extra Large? ").lower()
-    while s != "large" and s != "extra large" and s != "l" and s != "xl":
-        print("\nInvalid. Try Again\n")
-        s = input("Large or Extra Large? ").lower()
     
-    while z == 0:
-        try:
-            t = str(input("How many toppings "))
-            if float(t) in range(0, 5) and t.isnumeric() is True:
-                z = 1
-            else:
-                print("\nInvalid. Try Again\n")
-                t = t + t
-        except ValueError:
-            print("\nInvalid. Try Again\n")
+while True:
+    try:
+        t = str(input("How many toppings "))
+        if float(t) in range(0, 5) and t.isnumeric() is True:
+            t = float(t)
+            break
+        else:
+            print("\nInvalid. Please enter amount, 0-4.\n")
+    except ValueError:
+        print("\nInvalid. Not an amount.\n")
 
-    t = float(t)
+if s.casefold() == "large" or "l": 
+    st = 6
+if s.casefold() == "extra large" or "xl":
+    st = 10
+    print(st)
+if t != 0:
+    st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*(t) + 0.15), 2))
+tax = round(st*0.13, 2)
 
-    if s.casefold() == "large" or "l": 
-        st = 6
-    elif s.casefold() == "extra large" or "xl":
-        st = 10
-    if t != 0:
-        st = (round(st+(0.0167*(t**3) - 0.1*(t**2) + 0.9333*(t) + 0.15), 2))
-    tax = round(st*0.13, 2)
-
-    print('\n' + s + " Size ")
-    print(str(t) + " Toppings")
-    print("\nSubtotal    $" + str(st))
-    print("Tax         $" + str(tax))
-    print("Total       $" + str(round(float(st + tax), 2)))
-    break
+print('\n' * 3 + s + " Size ")
+print(int(t), "Toppings")
+print("\nSubtotal    $" + str(st))
+print("Tax         $" + str(tax))
+print("Total       $" + str(round(float(st + tax), 2)))
